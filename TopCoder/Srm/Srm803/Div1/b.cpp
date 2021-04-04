f65d91bb706a2e4faaa56f03aa4145c4887aab06
Dinh Nguyen Khoi <khoidinh@busmap.vn>
2021-04-04 23:07:17 +0700

    Do some exercises

--- TopCoder/Srm/Srm803/Div1/b.cpp
+++ TopCoder/Srm/Srm803/Div1/b.cpp
@@ -4,6 +4,8 @@ using namespace std;
 const int MAX_N = 3005;
 int dp[MAX_N][(1 << 12) + 5];
 vector <pair <char, int> > save;
+int next[MAX_N][26];
+pair <int, int> trace[MAX_N][(1 << 12) + 5];
 
 void update(int &a, int b) {
   a = max(a, b);
@@ -12,34 +14,17 @@ void update(int &a, int b) {
 class MarriageAndSelectionChallenge {
   public:
   string solve(string s) {
-    int n = (int) s.size();
-    int maxi = -1;
-    string ans = "";
-    for (char c = 'a'; c <= 'l'; c++) {
-      string tmp = "";
-      for (int i = 0; i < n; i++) {
-        if (s[i] == c) {
-          cnt++;
-          tmp += s[i];
+    reverse(s.begin(), s.end());
+    for (int i = 0; i < n; i++) {
+      for (int c = 0; c < 12; c++) {
+        next[i][c] = -1;
+      }
+      for (int j = i + 1; j <= n; j++) {
+        if (next[i][s[j - 1] - 'a'] == -1) {
+          next[i][s[j - 1] - 'a'] = j;
         }
       }
-      if (maxi < (int) tmp.size()) {
-        maxi = (int) tmp.size();
-        ans = tmp;
-      }
     }
-    s += '.';
-    int len = 1;
-    for (int i = 1; i <= n; i++) {
-      if (s[i] != s[i - 1]) {
-        save.push_back(make_pair(s[i - 1], len));
-        len = 1;
-      }
-      else {
-        len++;
-      }
-    }
-    n = (int) save.size();
     for (int i = 0; i <= n; i++) {
       for (int mask = 0; mask < (1 << 12); mask++) {
         dp[i][mask] = -1;
@@ -51,14 +36,28 @@ class MarriageAndSelectionChallenge {
         if (dp[i][mask] == -1) {
           continue;
         }
-        update(dp[i + 1][mask], dp[i][mask]);
-        int nxtChar = save[i].first - 'a';
-        if ((mask & (1 << nxtChar)) == 0) {
-          update(dp[i + 1][mask | (1 << nxtChar)], dp[i][mask] + save[i].second);
+        for (int c = 0; c < 12; c++) {
+          if (next[i][c] != -1) {
+            int nextPos = next[i][c];
+            if ((mask & (1 << c)) == 0) {
+              if (dp[nextPos][mask | (1 << c)] < dp[i][mask] + 1) {
+                dp[nextPos][mask | (1 << c)] = dp[i][mask] + 1;
+                trace[nextPos][mask | (1 << c)] = make_pair(i, mask);
+              }
+              else if (dp[nextPos][mask | (1 << c)] == dp[i][mask] + 1) {
+                if (s[trace[nextPos][mask | (1 << c)].first - 1] > s[i - 1]) {
+                  trace[nextPos][mask | (1 << c)] = make_pair(i, mask);
+                }
+              }
+            }
+            else if (i > 0 && s[i - 1] - 'a' == c) {
+              update(dp[nextPos][mask], dp[i][mask] + 1);
+            }
+          }
         }
       }
     }
-    return ans;
+
   }
 };
 
--- TopCoder/Srm/Srm803/Div1/sol.cpp
+++ TopCoder/Srm/Srm803/Div1/sol.cpp
@@ -1,86 +1,11 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-struct element {
-  int a[5];
-  bool operator < (const element &other) const {
-    return (a[0] < other.a[0] || a[0] == other.a[0] && a[1] < other.a[1] ||
-    a[0] == other.a[0] && a[1] == other.a[1] && a[2] < other.a[2] ||
-    a[0] == other.a[0] && a[1] == other.a[1] && a[2] == other.a[2] && a[3] < other.a[3] ||
-    a[0] == other.a[0] && a[1] == other.a[1] && a[2] == other.a[2] && a[3] == other.a[3] && a[4] < other.a[4]);
-  }
-  bool operator == (const element &other) const {
-    return (a[0] == other.a[0] && a[1] == other.a[1] && a[2] == other.a[2] &&
-    a[3] == other.a[3] && a[4] == other.a[4]);
-  }
-};
-queue <element> q;
-map <element, pair <element, int> > trace;
 
-void traceBack(element &e, element &start) {
-  element current = e;
-  string res = "";
-  while (true) {
-    cout << "ngu" << endl;
-    cout << current.a[0] << current.a[1] << current.a[2] << current.a[3] << current.a[4] << endl;
-    pair <element, int> back = trace[current];
-    res += (char) (back.second + '0');
-    current = back.first;
-    if (current == start) {
-      break;
-    }
-  }
-  reverse(res.begin(), res.end());
-  cout << res;
-}
+class {
+  public:
+};
 
 int main () {
-  element first;
-  first.a[0] = 7;
-  first.a[1] = 7;
-  first.a[2] = 9;
-  first.a[3] = 5;
-  first.a[4] = 7;
-  q.push(first);
-  while (true) {
-    element e = q.front();
-    q.pop();
-    for (int i = 0; i < 5; i++) {
-      element nxt = e;
-      nxt.a[i]++;
-      bool flag = true;
-      for (int j = 0; j < 5; j++) {
-        if (nxt.a[j] == 0) {
-          flag = false;
-          break;
-        }
-        if (j != i) {
-          nxt.a[j]--;
-        }
-      }
-      for (int j = 0; j < 5; j++) {
-        cout << nxt.a[j] << ' ';
-      }
-      cout << endl;
-      if (flag == true && trace.find(nxt) == trace.end()) {
-        trace[nxt] = make_pair(e, i);
-        q.push(nxt);
-        int sum = 0;
-        for (int j = 0; j < 5; j++) {
-          sum += nxt.a[j];
-        }
-        cout << sum << endl;
-        if (sum == 1) {
-          //traceBack(nxt, first);
-          cout << "YES" << endl;
-          return 0;
-        }
-      }
-    }
-  }
   return 0;
 }
-
-/*
-(a, b) -> (2 * a, b + 1
-*/
